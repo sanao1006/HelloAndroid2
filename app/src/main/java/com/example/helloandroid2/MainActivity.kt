fdf91d56b7f3ad7fb50d20d1685dc2c806199dd6
sanao1006 <naosak1006@gmail.com>
2023-02-08 22:54:23 +0900

    リスナクラス作った

--- app/src/main/java/com/example/helloandroid2/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid2/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.helloandroid2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+
+    private  inner class  HelloListner: View.OnClickListener {
+        override fun onClick(v: View?) {
+            TODO("Not yet implemented")
+        }
     }
 }
