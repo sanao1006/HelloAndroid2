34744799ef54c96fdc1b6cc157df6cd405695d2f
sanao1006 <naosak1006@gmail.com>
2023-02-09 20:04:46 +0900

    リスナ作った

--- app/src/main/java/com/example/helloandroid2/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.helloandroid2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
@@ -10,6 +11,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btClick = findViewById<Button>(R.id.btClick)
+
+        val listener = HelloListner()
+        btClick.setOnClickListener(listener)
 
     }
 
